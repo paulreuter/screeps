b6821140168edfc67c78403638c8cb4e21299e79
paulreuter <pr@helinks.com>
2018-01-27 23:07:59 +0100

    added conduit behaviour

---
+++ role.conduit.js
@@ -0,0 +1,67 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.conduit');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = {
+run: function(creep) {
+    if( creep.spawning)
+    {
+        return;
+    }
+    if( ! creep.memory.comfy)
+    {
+        var pos = creep.memory.myPos;
+        if( ! pos)
+        {
+            for( var i in creep.room.memory.conduits)
+            {
+                var claimed = creep.room.memory.conduits[i].creep;
+                if(claimed)
+                {
+                    var claimingCreep = Game.getObjectById(claimed);
+                    if(claimingCreep)
+                        continue;
+                }
+                creep.memory.pos = creep.room.memory.conduits[i].pos;
+                creep.memory.from = creep.room.memory.conduits[i].from;
+                creep.memory.to = creep.room.memory.conduits[i].to;
+                creep.room.memory.conduits[i].creep = creep.id;
+                break;
+            }
+        }
+        if( pos )
+        {
+            if( !creep.pos.inRangeTo(pos, 0) )
+            {
+                creep.moveTo(pos, {visualizePathStyle: {stroke: '#ffaa00'}});
+                return;
+            } else 
+            {
+                creep.memory.comfy = true;
+            }
+        }
+    } else 
+    {
+        if( creep.carry[RESOURCE_ENERGY] < creep.carryCapacity)
+        {
+            var from = Game.getObjectById( creep.memory.from );
+            if( from )
+            {
+                creep.withdraw(from, RESOURCE_ENERGY);
+            }
+        } else
+        {
+            var to = Game.getObjectById( creep.memory.to );
+            if( to )
+            {
+                creep.transfer(to, RESOURCE_ENERGY);
+            }
+        }
+    }
+}
+}
